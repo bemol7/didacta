1dae8dc1127574fd01f84f8dc7aa7f4375b54018
bemol7 <bemol.emol@gmail.com>
2015-07-17 09:10:33 -0500

    marco de aplicación

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,15 @@
+// get/ quizes/question
+exports.question = function(req,res){
+	res.render('quizes/question',{pregunta:'capital de italia'})
+};
+
+// get/ quizes/answer
+exports.answer = function(req,res){
+	if (req.query.respuesta=== 'Roma'){
+		res.render('quizes/answer',{respuesta:'Correcto'});
+	}
+
+	else {
+		res.render('quizes/answer',{respuesta:'Incorrecto'})
+	};
+};
